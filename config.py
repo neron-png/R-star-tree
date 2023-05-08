8cb52ec38957fc470c219ef651450d4c6b469e39
Neron-png <neronmkp@csd.auth.gr>
2023-05-08 12:55:22 +0300

    hi

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ BLOCKSIZE = 32000
 INPUTFILE = "map.osm"
 DATAFILE = "data.xml"
 INDEXFILE = "index.xml"
+
+FILEHEADER = """
+<header>
+<type>{0}</type>
+<records>0</records>
+<lastrecord>0</lastrecord>
+</header>
+"""
--- storageHandler.py
+++ storageHandler.py
@@ -7,14 +7,33 @@ def initialize():
     files = (DATAFILE, INDEXFILE)
 
     for file in files:
-        with open(file, "ab+") as cursor:
-            cursor.seek(0, 2)
-            print(cursor.tell())
+        cursor = open(file, "ab+")
+        # Getting the end of the file
+        cursor.seek(0, 2) # get to the end of file
+        
+        fileOccupancy = cursor.tell()
 
+        if fileOccupancy < BLOCKSIZE:
+            
+            initBlock = [0 for _ in range(BLOCKSIZE)]
+            initHeader = FILEHEADER.format(file)
+
+            for i in range(len(initHeader)):
+                initBlock[i] = initHeader[i]
+
+            initBlock = bytearray(initBlock)
+
+            cursor.close()
+            cursor = open(file, "wb+")
+            cursor.seek(0)
+            cursor.write(initBlock)
+        
+        cursor.close()
+            
 
 def fetchNextBlock():
     with open(INPUTFILE, "rb") as file:
-        yield file.read(BLOCKSIZE)
+        yield file.read(BLOCKSIZE).decode('utf-8')
 
 
 records = 0
