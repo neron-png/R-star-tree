56c910a9fcb2898138411a6ef043e03e0fbf28de
Neron-png <neronmkp@csd.auth.gr>
2023-07-24 20:41:46 +0300

    gamw

---
+++ deleteTemp.py
@@ -0,0 +1,5 @@
+
+
+def delete(nodes, nodeCap, id):
+    
+    pass
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import RTree
 
 
 if __name__ == "__main__":
-    # StorageHandler.write_blocks_to_datafile()
+    StorageHandler.write_blocks_to_datafile()
     StorageHandler.getBlockFromDisk(0)
-    # RTree.run()
+    RTree.run()
 
